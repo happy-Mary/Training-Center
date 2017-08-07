8d01d1c9a5dc124a4f88382b4c7489a9ee6faf44
happy-Mary <mariashklovska1991@gmail.com>
2017-08-07 17:29:43 +0300

    Models and views

--- BackboneProducts - Shklovskaya Maria/app/js/script.js
+++ BackboneProducts - Shklovskaya Maria/app/js/script.js
@@ -2,10 +2,14 @@
     var App = {
         Models: {},
         Views: {},
-        Collections: {}
+        Collections: {},
     };
 
-    window.template = function(id) {
+    var Products = {
+        user: {}
+    };
+
+    App.template = function(id) {
         return _.template($('#' + id).html());
     }
 
@@ -15,7 +19,10 @@
     App.Views.Product = Backbone.View.extend({
         tagName: 'div',
         className: 'item',
-        template: template('productTemplate'),
+        template: App.template('productTemplate'),
+        events: {
+            'click .open-but' : function(){console.log(this.model.get('title'));}
+        },
         render: function(gridClass) {
             var template = this.template(this.model.toJSON());
             this.$el.addClass(gridClass);
@@ -27,16 +34,34 @@
     // коллекция моделей товаров
     App.Collections.Product = Backbone.Collection.extend({
         model: App.Models.Product,
+        changeCategory: function(category){
+            var obj = JSON.parse(localStorage.getItem('goods'));
+            var goods = [];
+            if(category!=='all') {
+                for (var i = 0; i <= obj.length - 1; i++) {
+                    if (obj[i].category === category) {
+                        goods.push(obj[i]);
+                    }
+                }
+            } else {
+                goods = obj;
+            }
+            
+            this.set(goods);
+            buildGrid();
+        }
     });
 
     // коллекция видов товаров
     App.Views.Products = Backbone.View.extend({
         tagName: 'div',
         className: 'container',
-        render: function() {
+
+        render: function(paginate) {
+
             var all = this.collection.length;
-            var itemCount = parseInt($('input[name="paginate"]:checked').val());
-            // var itemCount = 6;
+        
+            var itemCount = parseInt(paginate) || parseInt($('input[name="paginate"]:checked').val());
             var gridCount = 12 / itemCount;
             // количество рядов
             var countRows = Math.ceil(all / itemCount);
@@ -46,6 +71,9 @@
                 for (var i = 1; i <= itemCount; i++) {
                     if (prod > all - 1) { this.$el.append(row); } else {
                         var product = this.collection.models[prod];
+                        // product set attributes
+                        this.checkAccess(product, Products.user);
+                        // console.log(product);
                         var productView = new App.Views.Product({ model: product });
                         var gridClass = "col-sm-" + gridCount;
                         var newItem = productView.render(gridClass).el;
@@ -57,34 +85,96 @@
             }
             return this;
         },
-        addOne: function(product) {
-            var newView = new App.Views.Product({ model: product });
-            this.$el.append(newView.render().el);
+
+        checkAccess: function(model, user){
+            // //////////////////////////////
+            model.set('readible', 'true');
+            model.set('rewritable', 'false');
+            model.set('removable', 'false');
+            model.set('addible', 'false');
+            // console.log(model.get('readible'));
+            if (user.role === 'admin') {
+                model.set('rewritable', 'true');
+                model.set('removable', 'true');
+                model.set('addible', 'true');
+            } else if(user.role === 'user'){
+                model.set('addible', 'true');
+            } else if(user.role === "user" && user.userId === model.get('self_id') || user.role === "admin" && user.userId === model.get('self_id')){
+                // для админа оставить только лэйбу
+                // itemOwn.css('display', 'block');
+                model.set('rewritable', 'true');
+                model.set('removable', 'true');
+            }
+             return model;
+            // //////////////////////////////
         }
+       
     });
 
 
-    // GET
-    function getG() {
-        $.ajax({
-            url: "json/goods.json",
-            success: function(result) {
-                console.log('result');
-                console.log(result);
-                App.Collections.productsCollection = new App.Collections.Product();
-                App.Collections.productsCollection.set(result);
-                console.log(App.Collections.productsCollection);
-                initApp();
-            }
-        });
+    // GET data on firs load
+    // not recommended to use fetch() on first time
+    function getGoodsFirstTime() {
+        Products.productsCollection = new App.Collections.Product();
+         // get user
+        Products.user = JSON.parse(localStorage.getItem('user'));
+        if (Products.user === null) {
+            Products.user = new User();
+            localStorage.setItem("user", JSON.stringify(Products.user));
+        }
+        // get goods
+        var goods = JSON.parse(localStorage.getItem('goods'));
+         if (goods === null){
+            $.ajax({
+                url: "json/goods.json",
+                success: function(result) {
+                    localStorage.setItem('goods', JSON.stringify(result));
+                    Products.productsCollection.set(result);
+                    buildGrid();
+                }
+            });
+         } else {
+            Products.productsCollection.set(goods);
+            buildGrid();
+        }
     }
 
-    function initApp() {
-        console.log(App.Collections.productsCollection);
-        var productsView = new App.Views.Products({ collection: App.Collections.productsCollection });
-        $('.items-goods').html(productsView.render().el);
+    function buildGrid() {
+        Products.productsView = new App.Views.Products({ collection: Products.productsCollection });
+        $('.items-goods').html(Products.productsView.render().el);
     }
 
-    getG();
-    console.log('done');
+    // first loading content
+    getGoodsFirstTime();
+
+    // внешние события
+    // change grid
+    $('.menu a').click(function() {
+            $('.menu a.active').removeClass('active');
+            $(this).addClass('active');
+
+            var category = $(this).html();
+            Products.productsCollection.changeCategory(category);
+    });
+    // grid pagination
+    var paginateRadio = $('input[name="paginate"]');
+    paginateRadio.change(function() { 
+        Products.productsView.remove(); 
+         Products.productsView = new App.Views.Products({ collection: Products.productsCollection });
+        $('.items-goods').html(Products.productsView.render().el); 
+    });
+
+
+    // User class
+    function User(id, name, password, role) {
+        // this.userId = id || 0;
+        this.userId = 100;
+        // this.name = name || "Anonim";
+        this.name = "Admin";
+        this.password = password || null;
+        // this.role = role || "anonim";
+        this.role = "admin";
+    }
+
+
 })();
