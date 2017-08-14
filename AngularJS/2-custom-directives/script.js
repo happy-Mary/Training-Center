56c45762bc02e6cb8961ac82179d958df525899f
happy-Mary <mariashklovska1991@gmail.com>
2017-08-14 18:14:37 +0300

    modal window directive

--- AngularJS/2-custom-directives/script.js
+++ AngularJS/2-custom-directives/script.js
@@ -1,2 +1,18 @@
 var myApp = angular.module('myApp', []);
+myApp.controller('mainCtrl', function($scope){
+    $scope.text = "I'm the main text and I'm glad you're reading me";
+    $scope.open = false;
+});
+
+myApp.directive('modal', function(){
+    return {
+        scope: {
+            'titleCountry': "@"
+        },
+        templateUrl: "modalTempl.html",
+        transclude: true,
+        replace: true
+    }
+});
+
 
