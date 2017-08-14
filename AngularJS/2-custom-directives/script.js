d535cd8a6cdff770fb39a7f5f27cdedcd24bef5c
happy-Mary <mariashklovska1991@gmail.com>
2017-08-14 22:33:00 +0300

    Callback for closing modal added

--- AngularJS/2-custom-directives/script.js
+++ AngularJS/2-custom-directives/script.js
@@ -1,18 +1,20 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('mainCtrl', function($scope){
+myApp.controller('mainCtrl', function($scope) {
     $scope.text = "I'm the main text and I'm glad you're reading me";
     $scope.open = false;
+    $scope.closeModal = function() {
+        $scope.open = false;
+    }
 });
 
-myApp.directive('modal', function(){
+myApp.directive('modal', function() {
     return {
         scope: {
-            'titleCountry': "@"
+            'titleCountry': "@",
+            'close': "&onClose"
         },
         templateUrl: "modalTempl.html",
         transclude: true,
         replace: true
     }
 });
-
-
