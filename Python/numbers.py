6a7615a89542df67e78fead882d7f76f08e72e73
happy-Mary <mariashklovska1991@gmail.com>
2017-10-02 21:50:53 +0300

    python/feature: counting days function

--- Python/numbers.py
+++ Python/numbers.py
@@ -1,10 +1,22 @@
-a = int(input('Enter first number: '))
-b = int(input('Enter second number: ')) 
+def getNum(a, b):
+    
+    if not isinstance(a, int) or not isinstance(b, int):
+        print('Not numbers')
+        return
 
-if a%2 == 0 and b%2 == 0:
-    print(a * b)
-elif a%2 != 0 and b%2 != 0:
-    print(a + b)
-else:
-    x = a if a%2 != 0 else b
-    print(x)
+    if a%2 == 0 and b%2 == 0:
+        print(a * b)
+    elif a%2 != 0 and b%2 != 0:
+        print(a + b)
+    else:
+        x = a if a%2 != 0 else b
+        print(x)
+    return
+
+getNum(2, 4)
+getNum(7, 3)
+getNum(7, 4)
+getNum(4, 9)
+getNum('b', 9)
+getNum(4, 'c')
+getNum('f', 'd')
