9ed854c4ba0888b06619384f31465bfd6f0e57f5
happy-Mary <mariashklovska1991@gmail.com>
2017-10-11 16:18:42 +0300

    python/feature: deleted nested for

--- Python/1.vowels.py
+++ Python/1.vowels.py
@@ -4,24 +4,11 @@ def checkVowels(string_user):
     vowels = 0
 
     for letter in string_user:
-       for vowel in vowelsLat:
-            if letter is vowel:
-                print(letter)
-                vowels+=1
-
-#    for vowel in vowelsLat:
-#        count = string_user.count(vowel)
-#        vowels += count
+        if letter in vowelsLat:
+            print(letter)
+            vowels+=1
 
     print('Wovels in your string: ', vowels)
 
-def askForString():
-    string = input('Enter string: ')
-    if len(string) <= 0:
-        print(len(string))
-        askForString()
-    else:
-        checkVowels(string)
-
-
-askForString()
+string = input('Enter string: ')
+checkVowels(string)
