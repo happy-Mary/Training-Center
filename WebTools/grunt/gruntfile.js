466c650156a6c1ee2826a031f753582193fd4c83
happy-Mary <mariashklovska1991@gmail.com>
2017-11-13 18:36:00 +0300

    webtools/feature: using grunt with browserify

--- WebTools/grunt/gruntfile.js
+++ WebTools/grunt/gruntfile.js
@@ -1,33 +1,33 @@
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        jshint: {
-            files: ['gruntfile.js', 'src/**/*.js']
-        },
-        jsdoc: {
-            dist: {
-                src: ['src/**/*.js'],
-                options: {
-                    destination: 'doc'
-                }
-            }
-        },
-        postcss: {
-            options: {
-                map: true,
+  grunt.initConfig({
+      jshint: {
+          files: ['gruntfile.js', 'src/**/*.js']
+      },
+      jsdoc: {
+          dist: {
+              src: ['src/**/*.js'],
+              options: {
+                  destination: 'doc'
+              }
+          }
+      },
+      postcss: {
+          options: {
+              map: true,
 
-                processors: [
-                    require('pixrem')(),
-                    require('autoprefixer')({browsers: 'last 2 versions'}),
-                    require('cssnano')()
-                ]
-            },
-            dist: {
-                src: 'src/*.css'
-            }
-        },
-    });
+              processors: [
+                  require('pixrem')(),
+                  require('autoprefixer')({browsers: 'last 2 versions'}),
+                  require('cssnano')()
+              ]
+          },
+          dist: {
+              src: 'src/*.css'
+          }
+      },
+  });
 
-    grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint']);
 }
