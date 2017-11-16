3e467534f95ac62487881e70fcc7b061043506b7
happy-Mary <mariashklovska1991@gmail.com>
2017-11-16 16:04:52 +0300

    webtools/feature: counting time execution throug saving to file

--- WebTools/grunt-custom/gruntfile.js
+++ WebTools/grunt-custom/gruntfile.js
@@ -4,41 +4,76 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         tasksTime: {
-            firstTask: { start: 0, stop: 2 },
-            secondTask: { start: 0, stop: 4 }
+            firstTask: { start: 0, stop: 0 },
+            secondTask: { start: 0, stop: 0 }
         },
         browserify: {
             dist: {
                 files: {
-                  'build-script/script-grunt.js': ['src/js/**/*.js']
+                  'build/script-grunt.js': ['src/js/**/*.js']
                 }
             }
         }
     });
 
-    grunt.registerTask('register-time', function(opt) {
+    function setStartTime(time) {
+        var firstTaskStart = grunt.config.get('tasksTime.firstTask.start');
+        if (!firstTaskStart) {
+            grunt.config.set('tasksTime.firstTask.start', time);
+        } else {
+            grunt.config.set('tasksTime.secondTask.start', time);
+        }
+    }
+
+    function setStopTime(time) {
+        var firstTaskStop = grunt.config.get('tasksTime.firstTask.stop');
+        if (!firstTaskStop) {
+            grunt.config.set('tasksTime.firstTask.stop', time);
+        } else {
+            grunt.config.set('tasksTime.secondTask.stop', time);
+        }
+    }
+
+    grunt.registerTask('write-time-file', function() {
         var currTime = new Date().getTime();
+        grunt.file.write('data.txt', currTime);
+    });
+
+    grunt.registerTask('read-time-file', function() {
+        var timeStart = grunt.file.read('data.txt');
+        grunt.file.delete('data.txt');
+        grunt.task.run('register-time:start:' + timeStart);
+    });
+
+    grunt.registerTask('register-time', function(opt, val) {
+        var currTime = val || new Date().getTime();
         if (opt === 'start') {
-            grunt.log.writeln('COUNT START ' + currTime);
-            grunt.config.set('tasksTime.firstTask.start', currTime);
-            grunt.option('test', 'good');
+            setStartTime(currTime);
         }
         else if (opt === 'stop')  {
-            grunt.log.writeln('COUNT STOP ' + currTime);
-            grunt.config.set('tasksTime.firstTask.stop', currTime);
+            setStopTime(currTime);
         }
     });
 
     grunt.registerTask('count-time', function() {
-        grunt.log.writeln(grunt.option('test'));
-        grunt.log.writeln('firstTaskStart: ' + grunt.config.get('tasksTime.firstTask.start'));
-        grunt.log.writeln('firstTaskEnd: ' + grunt.config.get('tasksTime.firstTask.stop'));
-        grunt.log.writeln('secondTaskStart: ' + grunt.config.get('tasksTime.secondTask.start'));
-        grunt.log.writeln('secondTaskEnd: ' + grunt.config.get('tasksTime.secondTask.stop'));
+        var firstTaskStart = grunt.config.get('tasksTime.firstTask.start');
+        var firstTaskStop = grunt.config.get('tasksTime.firstTask.stop');
+        var secondTaskStart = grunt.config.get('tasksTime.secondTask.start');
+        var secondTaskStop = grunt.config.get('tasksTime.secondTask.stop');
+
+        var firstTaskTime = firstTaskStop - firstTaskStart;
+        var secondTaskTime = secondTaskStop - secondTaskStart;
+
+        grunt.log.writeln('firstTaskExec: ' + firstTaskTime);
+        grunt.log.writeln('secondTaskExec: ' + secondTaskTime);
+
+        if (firstTaskTime > secondTaskTime) {
+            grunt.log.ok('GRUNT-BROWSERIFY is faster');
+        } else {
+            grunt.log.ok('BROWSERIFY is faster');
+        }
     });
 
-    grunt.registerTask('default', ['register-time:start', 'count-time', 'register-time:stop', 'count-time']);
-
-    grunt.registerTask('start', ['register-time:start']);
-    grunt.registerTask('finish', ['register-time:stop', 'count-time']);
+    grunt.registerTask('start', ['write-time-file']);
+    grunt.registerTask('finish', ['register-time:stop', 'read-time-file', 'register-time:start', 'browserify', 'register-time:stop', 'count-time']);
 };
