673fd15801ef1b94f5948bc2b3bd7a99cb2428b7
happy-Mary <mariashklovska1991@gmail.com>
2018-07-27 17:03:14 +0300

    addons

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -15,4 +15,12 @@ function getNum(num) {
     return arr.join(' + ');
 }
 
+// const expandedForm = n => n.toString()
+//                             .split("")
+//                             .reverse()
+//                             .map( (a, i) => a * Math.pow(10, i))
+//                             .filter(a => a > 0)
+//                             .reverse()
+//                             .join(" + ");
+
 getNum(70304);
